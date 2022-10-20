b4026e01b43d6f005c875510dd49054e51bc6f21
akundu3387 <116226377+akundu3387@users.noreply.github.com>
2022-10-19 23:10:57 -0400

    Create mytest.cpp
    test file, contains main()

---
+++ mytest.cpp
@@ -0,0 +1,21 @@
+#include "graph.h"
+int main(){
+
+    Graph aGraph("data.txt");
+    aGraph.findPath(0,5);
+    cout << "Finding the path from node 0 to node 5: " << endl;
+    aGraph.dump();
+
+    aGraph.findPath(2,2);
+    cout << "Finding the path from node 2 to itself: " << endl;
+    aGraph.dump();
+    aGraph.findPath(4,0);
+    cout << "Finding the path from node 4 to node 0: " << endl;
+    aGraph.dump();
+
+    Graph bGraph;
+    bGraph.findPath(1,14);
+    cout << "Finding the path from node 1 to node 14 in an empty Graph object: " << endl;
+    bGraph.dump();
+    return 0;
+}
